6b268f5e95deba51d0c7cd78b61035a840c991d4
TheDevCodingKen <93670654+TheDevCodingKen@users.noreply.github.com>
2022-06-13 11:50:28 -0700

    initial commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const topSpotsData = require('../server/data.json');
 
+// create Express server below
+const app = express();
+// Make Morgan log each request using the pre-defined 'dev' format
+app.use(morgan('dev'));
 
-// create your express server below
-var app;
+// Routes HTTP GET requests to the specified path "/" with the specified callback function
+app.get('/', function(req, res) {
+    res.status(200).send('Hello');
+});
+app.get('/data', function(req, res) {
+    res.status(200).json(topSpotsData);
+});
+app.get('*', function(req, res){
+    res.end('Not Found');
+});
 
-// add your routes and middleware below
-
-// finally export the express application
+// Export the Express application
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const server = require('./app');
 
 // We're telling the app to listen for network requests on port 3000
-server.listen(3000, function() {
-  console.log('Server is listening on http://localhost:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, function() {
+  console.log(`Server is listening on localhost: ${port}`);
 });
